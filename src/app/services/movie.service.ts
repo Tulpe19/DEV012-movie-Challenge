f7f130391cac27b79a51376db31ab57d907f9916
Juliana Guzmán <juliguzmac1906@gmail.com>
2024-01-12 19:40:08 -0500

    Se importa el modulo httpClient y se inicia con el servicio para traer la galeria de peliculas

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ResponseApi } from '../interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDFhM2U4ZDg4MGVhYmExYjM3OTgwZTY5MWU2NjU3OCIsInN1YiI6IjY1Nzg3YmQ2ODlkOTdmMDBlMzc1OTM3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.By9BSHjUwo1V8u14rY-s76sizKT4KpIzrqTT4BabX5U";
+  ENDPOINT_BASE = "https://api.themoviedb.org/3";
+  URL_GENRE_MOVIES = "";
+  constructor(private httpClient: HttpClient) { }
+
+  public getData(){
+    const urlMovieGallery = this.httpClient.get(`${this.API_KEY} `)
+
+  }
+
 }
